55a275375552be6dd7fc214d343818a15446f580
Nehan1901 <121910316027@gitam.in>
2026-06-07 23:24:59 -0400

    made chnages in cors for the backend deployed url

--- backend/main.py
+++ backend/main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://docmind-ebon.vercel.app",
-        "https://docmind-production.up.railway.app",
+        "https://docmindai.up.railway.app",
         "http://localhost:5173",
         "http://localhost:5174",
     ],
